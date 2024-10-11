97bf2dac7f95f02ff723f4e33dce684541b984be
Nguyen Trong Hieu <tronghieunguyen87@gmail.com>
2024-10-11 22:25:59 +0700

    Initial commit

---
+++ WebHRM/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+/*< !--JavaScript để điều khiển mở rộng / thu nhỏ Sidebar-- >*/
+function toggleSidebar() {
+    var sidebar = document.getElementById("sidebar");
+    var mainContent = document.getElementById("mainContent");
+
+    // Kiểm tra trạng thái Sidebar và chuyển đổi class 'collapsed'
+    sidebar.classList.toggle("collapsed");
+    mainContent.classList.toggle("collapsed");
+}
